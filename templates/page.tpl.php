87f94070388d3f6525e28c3cf28843c8e86db10d
Robert <robert@dpsg-heisingen.de>
2012-02-28 13:51:06 +0100

    Layout für 980px-Grid

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -107,8 +107,6 @@
         </div>
 	<?php endif; ?>
 
-	<!-- Diashow hier!! -->
-	
 	<?php if ($messages): ?>
 		<div id="messages">
 			<?php print $messages; ?>
@@ -117,6 +115,26 @@
 
 	<div id="main">
 
+		<div id="content">
+			<?php if ($breadcrumb): ?>
+				<div id="breadcrumb"><?php print $breadcrumb; ?></div>
+				<hr />
+			<?php endif; ?>
+		
+      		<div id="inner-content" class="column">
+        		<a id="main-content"></a>
+        		<?php print render($title_prefix); ?>
+        		<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+        		<?php print render($title_suffix); ?>
+        		<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        		<?php print render($page['help']); ?>
+        		<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+        		<?php print render($page['content']); ?>
+        		<?php print $feed_icons; ?>
+      		</div> <!-- /#inner-content -->
+		</div> <!-- /#content -->
+    	
+    	
     	<?php if ($page['sidebar_first']): ?>
         	<div id="sidebar-first" class="column sidebar">      		
           		<?php print render($page['sidebar_first']); ?>
@@ -128,25 +146,6 @@
           		<?php print render($page['sidebar_second']); ?>
         	</div> <!-- /#sidebar-second -->
       	<?php endif; ?>
-
-	<div id="content">
-		<?php if ($breadcrumb): ?>
-			<div id="breadcrumb"><?php print $breadcrumb; ?></div>
-			<hr />
-		<?php endif; ?>
-		
-      	<div id="inner-content" class="column">
-        	<a id="main-content"></a>
-        	<?php print render($title_prefix); ?>
-        	<?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-        	<?php print render($title_suffix); ?>
-        	<?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        	<?php print render($page['help']); ?>
-        	<?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-        	<?php print render($page['content']); ?>
-        	<?php print $feed_icons; ?>
-      	</div> <!-- /#inner-content -->
-	</div> <!-- /#content -->
 
 	</div><!-- /#main-->
 
---
+++ theme-settings.php
@@ -0,0 +1,10 @@
+<?php
+/*
+
+function DPSG_settings($saved_settings) {
+	
+	$settings = array_merge($defaults, $saved_settings);
+	
+}
+*/
+?>
